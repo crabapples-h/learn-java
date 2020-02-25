26119e78f42ed3e748b7ac35482c6384a8920311
hequan <hequan@gogpay.cn>
2020-02-25 19:15:12 +0800

    nacos测试

--- src/main/java/cn/crabapples/spring/DemoApplication.java
+++ src/main/java/cn/crabapples/spring/DemoApplication.java
@@ -1,10 +1,18 @@
 package cn.crabapples.spring;
 
+import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySources;
+import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * TODO springBoot主启动类
@@ -17,6 +25,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @EnableJpaAuditing
 @SpringBootApplication
+@NacosPropertySource(dataId = "learn-dev-yml",autoRefreshed = true)
+@EnableNacosConfig
+@EnableNacosDiscovery
 public class DemoApplication {
     private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
     public static void main(String[] args) {
