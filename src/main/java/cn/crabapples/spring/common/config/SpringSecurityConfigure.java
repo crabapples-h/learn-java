7e8840d3952de7d8500215b440697409e362b99e
crabapple <wishforyou.xia@gmail.com>
2020-03-04 01:00:20 +0800

    测试SpringSecurity

---
+++ src/main/java/cn/crabapples/spring/common/config/SpringSecurityConfigure.java
@@ -0,0 +1,16 @@
+package cn.crabapples.spring.common.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
+
+/**
+ * TODO
+ *
+ * @author Mr.He
+ * 2020/3/4 0:54
+ * e-mail crabapples.cn@gmail.com
+ * qq 294046317
+ * pc-name 29404
+ */
+public class SpringSecurityConfigure extends WebSecurityEnablerConfiguration {
+}
