8ff60127044dbb936f1a9d5ac6bc5b135d66f1df
crabapple <wishforyou.xia@gmail.com>
2020-03-04 00:34:30 +0800

    baseController删除多余代码

--- src/main/java/cn/crabapples/spring/common/BaseController.java
+++ src/main/java/cn/crabapples/spring/common/BaseController.java
@@ -4,7 +4,6 @@ import cn.crabapples.spring.dto.ResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
