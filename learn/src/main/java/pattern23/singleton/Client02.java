6b51d6ad14a5b8b075bc665f38a569252b968137
MrHe <crabapples.cn@gmail.com>
2020-06-09 15:37:16 +0800

    vue router

--- learn/src/main/java/pattern23/singleton/Client02.java
+++ learn/src/main/java/pattern23/singleton/Client02.java
@@ -18,13 +18,14 @@ import java.lang.reflect.Constructor;
  */
 public class Client02 {
     static Logger logger = LoggerFactory.getLogger(Client02.class);
+
     public static void main(String[] args) throws Exception {
         Singleton06 singleton06_1 = Singleton06.getInstance();
         Singleton06 singleton06_2 = Singleton06.getInstance();
-        logger.info("Hello:{}",singleton06_1);
-        logger.info("Hello:{}",singleton06_2);
+        logger.info("Hello:{}", singleton06_1);
+        logger.info("Hello:{}", singleton06_2);
 
-        new Thread(()->{
+        new Thread(() -> {
             try {
                 reflectDemo();
             } catch (Exception e) {
@@ -32,7 +33,7 @@ public class Client02 {
             }
         }).start();
 
-        new Thread(()->{
+        new Thread(() -> {
             try {
                 deserializationDemo();
             } catch (Exception e) {
@@ -43,24 +44,26 @@ public class Client02 {
 
     /**
      * 通过反射破解单例模式
+     *
      * @throws Exception 反射异常
      */
     public static void reflectDemo() throws Exception {
         Class<Singleton06> clazz = (Class<Singleton06>) Class.forName("cn.crabapples.pattern23.singleton.Singleton06");
-        Constructor<Singleton06> constructors = clazz.getDeclaredConstructor(null);
+        Constructor<Singleton06> constructors = clazz.getDeclaredConstructor((Class<?>) null);
         constructors.setAccessible(true);
         Singleton06 singleton06_3 = constructors.newInstance();
-        logger.info("Hello:{}",singleton06_3);
+        logger.info("Hello:{}", singleton06_3);
     }
 
 
     /**
      * 通过反序列化破解单例模式
+     *
      * @throws Exception 反序列化异常
      */
     public static void deserializationDemo() throws Exception {
         Singleton06 singleton06_1 = Singleton06.getInstance();
-        logger.info("Hello:{}",singleton06_1);
+        logger.info("Hello:{}", singleton06_1);
         File file = new File("SingletonDemoFile.temp");
         FileOutputStream fileOutputStream = new FileOutputStream(file);
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
@@ -69,6 +72,6 @@ public class Client02 {
         FileInputStream fileInputStream = new FileInputStream(file);
         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
         Singleton06 singleton06_2 = (Singleton06) objectInputStream.readObject();
-        logger.info("Hello:{}",singleton06_2);
+        logger.info("Hello:{}", singleton06_2);
     }
 }
--- web-page/src/main.js
+++ web-page/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Router from 'router'
 
 Vue.config.productionTip = false
-
+Vue.use(
+    VueAxios,
+    axios,
+    Router,
+)
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
