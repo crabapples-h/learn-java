9f8841da76d910adde4a3333dabab3fcdff65583
hequan <crabapples.cn@gmailcom>
2021-10-11 02:19:35 +0800

    add MouseListener

--- frontend-react/src/views/MouseListener.js
+++ frontend-react/src/views/MouseListener.js
@@ -4,25 +4,26 @@ import PropTypes from 'prop-types'
 class MouseListener extends React.Component {
     constructor(props) {
         super(props);
-
-
     }
+
     state = {
         x: 0,
         y: 0,
     }
 
-    setMouseLocation =(e)=>{
+    setMouseLocation = (e) => {
         this.setState({
-            x:e.clientX,
-            y:e.clientY
+            x: e.clientX,
+            y: e.clientY
         })
     }
+
     componentDidMount() {
-        window.addEventListener('mousemove',this.setMouseLocation)
+        window.addEventListener('mousemove', this.setMouseLocation)
     }
+
     componentWillUnmount() {
-        window.removeEventListener('mousemove',this.setMouseLocation)
+        window.removeEventListener('mousemove', this.setMouseLocation)
     }
 
     render() {
@@ -30,12 +31,12 @@ class MouseListener extends React.Component {
     }
 }
 
-MouseListener.propTypes={
-    children:PropTypes.func
+MouseListener.propTypes = {
+    children: PropTypes.func
 }
 
-MouseListener.defaultProps={
-    children:(e)=>{
+MouseListener.defaultProps = {
+    children: (e) => {
         console.log("未传入对应渲染函数，使用默认模式渲染")
         return <p>x:{e.x},y:{e.y}</p>
     }
