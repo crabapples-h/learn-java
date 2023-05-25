7afc92936883a48087218bc287bf883f12af91f6
mshe <crabapples.cn@gmail.com>
2023-05-25 23:22:28 +0800

    Revert "数据恢复后多余文件删除"
    This reverts commit 49cfefa7e004814a4e9a2cee33a9281423a54145.

---
+++ boot-jpa-notshiro/src/main/java/cn/crabapples/spring/system/common/config/FastJsonConfigure.java
@@ -0,0 +1,31 @@
+package cn.crabapples.spring.system.common.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * TODO fastJson配置
+ *
+ * @author Mr.He
+ * 2020/1/31 0:12
+ * e-mail crabapples.cn@gmail.com
+ * qq 294046317
+ * pc-name 29404
+ */
+@Configuration
+public class FastJsonConfigure {
+    @Bean
+    public FastJsonConfig fastJsonConfig() {
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss E");
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.WriteEnumUsingToString,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteDateUseDateFormat);
+        return fastJsonConfig;
+    }
+}
