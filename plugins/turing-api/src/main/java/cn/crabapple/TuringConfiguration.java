5f62ec0e66a86381e1b7f20f4364f88299318e8c
mshe <crabapples.cn@gmail.com>
2024-11-16 01:32:13 +0800

    refactor(turing-api): 重构图灵 API 配置
    - 更新 .gitignore 文件，添加 target 目录
    - 修改 spring.factories 文件，更新配置类路径- 更新 spring-configuration-metadata.json，精简配置属性
    - 重构 TuringConfiguration 类，移除冗余的 Bean 定义

--- plugins/turing-api/src/main/java/cn/crabapple/TuringConfiguration.java
+++ plugins/turing-api/src/main/java/cn/crabapple/TuringConfiguration.java
@@ -2,24 +2,24 @@ package cn.crabapple;
 
 import cn.crabapple.turing.TuringApiProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration //开启配置
-@ConditionalOnClass(TuringConfiguration.class)
-@EnableConfigurationProperties(TuringConfiguration.class) //开启使用映射实体对象
+@ConditionalOnClass(TuringApiProperties.class)
+@EnableConfigurationProperties(TuringApiProperties.class) //开启使用映射实体对象
 @ConditionalOnProperty(//存在对应配置信息时初始化该配置类
         prefix = "crabapples.turing",//存在配置前缀
         value = "enabled",//开启
         matchIfMissing = true//缺失检查
 )
 public class TuringConfiguration {
-    @Bean
-    @ConditionalOnMissingBean
-    public TuringApiProperties configProperties(TuringApiProperties properties) {
-        return properties;
-    }
+    // 这里如果再次注入bean的话会导致存在两个Bean，spring启动时会无法识别使用哪一个bean
+//    @Bean
+//    @ConditionalOnMissingBean
+//    public TuringApiProperties configProperties(TuringApiProperties properties) {
+//        System.err.println(123);
+//        return properties;
+//    }
 }
