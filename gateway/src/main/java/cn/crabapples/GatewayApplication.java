822760f8f683c0d1f126a7b5e080cc3362706871
mshe <crabapples.cn@gmail.com>
2024-11-18 23:23:51 +0800

    feat(gateway): 添加 Nacos服务发现和 Gateway 路由功能
    - 在 gateway 模块中集成 Nacos 服务发现- 添加 Gateway 路由配置，支持负载均衡
    - 新增 Nacos 模块，实现 Nacos 服务的独立部署
    - 更新项目配置，支持 Nacos 和 Gateway

--- gateway/src/main/java/cn/crabapples/GatewayApplication.java
+++ gateway/src/main/java/cn/crabapples/GatewayApplication.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
+//    @Bean
+//    @LoadBalanced
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
 }
