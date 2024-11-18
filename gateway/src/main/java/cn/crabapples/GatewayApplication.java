2125b6b50da52087e66ccac8ad2d8928578e44e7
mshe <crabapples.cn@gmail.com>
2024-11-19 00:30:55 +0800

    feat(gateway): 添加负载均衡功能
    - 在 GatewayApplication 中添加了 LoadBalanced 的 RestTemplate
    - 在 gateway 和 server 的 pom.xml 中添加了相关依赖
    - 更新了根 pom.xml，统一了版本管理

--- gateway/src/main/java/cn/crabapples/GatewayApplication.java
+++ gateway/src/main/java/cn/crabapples/GatewayApplication.java
@@ -2,17 +2,20 @@ package cn.crabapples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-//    @Bean
-//    @LoadBalanced
-//    public RestTemplate restTemplate() {
-//        return new RestTemplate();
-//    }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
