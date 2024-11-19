71408a829a586253a54c7087e329f67d1d6adfdd
mshe <666666666@666666666.666666666>
2024-11-19 16:05:47 +0800

    refactor(code-generator): 注释掉 ControllerTemplate 类的代码
    - 注释掉了整个 ControllerTemplate 类的代码，包括导入语句、类定义和所有方法
    - 保留了部分注释，以便未来可能的代码恢复或参考

--- plugins/code-generator/src/main/java/cn/crabapples/generator/ControllerTemplate.java
+++ plugins/code-generator/src/main/java/cn/crabapples/generator/ControllerTemplate.java
@@ -1,43 +1,43 @@
-package cn.crabapples.generator;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-
-
-//@RestController
-//@Slf4j
-@RequestMapping("/api/")
-public class ControllerTemplate extends BaseController {
-
+//package cn.crabapples.generator;
 //
-//    @PostMapping("/save")
-//    public ResponseDTO<Boolean> save() {
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import java.util.List;
+//
+//
+////@RestController
+////@Slf4j
+//@RequestMapping("/api/")
+//public class ControllerTemplate extends BaseController {
+//
+////
+////    @PostMapping("/save")
+////    public ResponseDTO<Boolean> save() {
+////        return new ResponseDTO<>();
+////    }
+////
+////    @GetMapping("/page")
+////    public ResponseDTO<Page> page(
+////            @RequestParam(value = "pageIndex", defaultValue = "1") Integer pageIndex,
+////            @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize) {
+////        return new ResponseDTO<>();
+////    }
+////
+//    @GetMapping("/list")
+////    @JwtIgnore
+//    public ResponseDTO<List> list() {
 //        return new ResponseDTO<>();
-//    }
 //
-//    @GetMapping("/page")
-//    public ResponseDTO<Page> page(
-//            @RequestParam(value = "pageIndex", defaultValue = "1") Integer pageIndex,
-//            @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize) {
-//        return new ResponseDTO<>();
 //    }
-//
-    @GetMapping("/list")
-//    @JwtIgnore
-    public ResponseDTO<List> list() {
-        return new ResponseDTO<>();
-
-    }
-//
-//    @GetMapping("/get/{id}")
-//    public ResponseDTO info(@PathVariable String id) {
-//        return new ResponseDTO<>();
-//    }
-//
-//    @DeleteMapping("/remove/{id}")
-//    public ResponseDTO<Boolean> remove(@PathVariable String id) {
-//        return new ResponseDTO<>();
-//    }
-}
+////
+////    @GetMapping("/get/{id}")
+////    public ResponseDTO info(@PathVariable String id) {
+////        return new ResponseDTO<>();
+////    }
+////
+////    @DeleteMapping("/remove/{id}")
+////    public ResponseDTO<Boolean> remove(@PathVariable String id) {
+////        return new ResponseDTO<>();
+////    }
+//}
