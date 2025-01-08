1a270bc8d8f6eeaa62e46ff5c5e0367010631e4d
mshe <crabapples.cn@gmail.com>
2025-01-08 22:14:43 +0800

    feat(server): 添加 MyBatis Mapper XML 文件生成功能
    - 新增 MyBatis Mapper XML 文件的模板和生成逻辑
    - 修改现有 Java 类生成方法，使用新的模板- 优化代码结构，提高可读性和可维护性

--- server/src/main/java/cn/crabapples/common/utils/thymeleaf/Main.java
+++ server/src/main/java/cn/crabapples/common/utils/thymeleaf/Main.java
@@ -7,20 +7,20 @@ import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.text.MessageFormat;
 import java.util.*;
 
 @Slf4j
 public class Main {
     private final static Map<String, String> TEMPLATE_MAP = new HashMap<>();
-    private final static String FILE_NAME_TEMPLATE = "{0}/{1}.java";
+    private final static String CLASS_NAME_TEMPLATE = "{0}/{1}.java";
+    private final static String XML_NAME_TEMPLATE = "{0}/{1}.xml";
     private final static String FILE_PATH_TEMPLATE = "{0}{1}";
+
 
     static {
         TEMPLATE_MAP.put("$$Controller", "controller");
@@ -56,12 +56,13 @@ public class Main {
     }
 
     private static void generatorMybatisMapper(Context context, String packageName, String moduleName) throws IOException {
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setPrefix("/templates/");
-        templateResolver.setSuffix(".cxt");
-        templateResolver.setTemplateMode("XML");
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".cxt");
+        resolver.setTemplateMode("TEXT");
+
         TemplateEngine engine = new TemplateEngine();
-        engine.setTemplateResolver(templateResolver);
+        engine.setTemplateResolver(resolver);
 
         String separator = FileSystems.getDefault().getSeparator();
         String path = "server" + separator +
@@ -72,28 +73,26 @@ public class Main {
                 "crabapples" + separator +
                 "custom" + separator +
                 packageName + separator;
-        for (Map.Entry<String, String> item : TEMPLATE_MAP.entrySet()) {
-            String templateName = item.getKey();
-            String subPackageName = item.getValue();
-            String filePath = MessageFormat.format(FILE_PATH_TEMPLATE, path, subPackageName);
-            File directory = new File(filePath);
-            directory.mkdirs();
-            String fileName = MessageFormat.format(FILE_NAME_TEMPLATE, filePath, templateName.replace("$$", moduleName));
-            File file = new File(fileName);
-            log.info("生成文件:[{}]", fileName);
-            OutputStreamWriter writer = new OutputStreamWriter(Files.newOutputStream(file.toPath()));
-            engine.process(templateName, context, writer);
-
-        }
+        String templateName = "$$Mapper";
+        String subPackageName = "dao/mybatis/xml";
+        String filePath = MessageFormat.format(FILE_PATH_TEMPLATE, path, subPackageName);
+        File directory = new File(filePath);
+        directory.mkdirs();
+        String fileName = MessageFormat.format(XML_NAME_TEMPLATE, filePath, templateName.replace("$$", moduleName));
+        File file = new File(fileName);
+        log.info("生成文件:[{}]", fileName);
+        OutputStreamWriter writer = new OutputStreamWriter(Files.newOutputStream(file.toPath()));
+        engine.process(templateName, context, writer);
     }
 
     private static void generatorJavaClass(Context context, String packageName, String moduleName) throws IOException {
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setPrefix("/templates/");
-        templateResolver.setSuffix(".cjt");
-        templateResolver.setTemplateMode("TEXT");
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".cjt");
+        resolver.setTemplateMode("TEXT");
+
         TemplateEngine engine = new TemplateEngine();
-        engine.setTemplateResolver(templateResolver);
+        engine.setTemplateResolver(resolver);
 
         String separator = FileSystems.getDefault().getSeparator();
         String path = "server" + separator +
@@ -110,7 +109,7 @@ public class Main {
             String filePath = MessageFormat.format(FILE_PATH_TEMPLATE, path, subPackageName);
             File directory = new File(filePath);
             directory.mkdirs();
-            String fileName = MessageFormat.format(FILE_NAME_TEMPLATE, filePath, templateName.replace("$$", moduleName));
+            String fileName = MessageFormat.format(CLASS_NAME_TEMPLATE, filePath, templateName.replace("$$", moduleName));
             File file = new File(fileName);
             log.info("生成文件:[{}]", fileName);
             OutputStreamWriter writer = new OutputStreamWriter(Files.newOutputStream(file.toPath()));
