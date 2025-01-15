84d13d3f781e3f071e85ff1ac473f714269d2bb6
mshe <666666666@666666666.666666666>
2025-01-15 14:54:24 +0800

    style(frontend-react): 登录页面样式调整
    - 在登录页面引入

--- frontend/frontend-react/vite.config.ts
+++ frontend/frontend-react/vite.config.ts
@@ -12,11 +12,11 @@ export default defineConfig({
       'components': path.resolve(__dirname, './src/components'), // 设置 'components' 指向 './src/components' 目录
       // 可以继续添加更多别名
     },
-    css: {
-      preprocessorOptions: {
-        less: {
-          javascriptEnabled: true, // 启用 JavaScript 支持（某些组件库可能需要）
-        },
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true, // 启用 JavaScript 支持（某些组件库可能需要）
       },
     },
   },
