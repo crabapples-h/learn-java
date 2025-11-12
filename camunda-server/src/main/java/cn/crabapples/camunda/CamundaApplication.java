753bb0bcb8f955cad8ba549260fd01d2514d0049
mshe <666666666@666666666.666666666>
2025-11-13 05:44:53 +0800

    feat(gateway): 修改网关启动端口并集成nacos自动启停功能- 将网关服务端口从9092更改为9093 - 在应用启动时自动执行docker-compose命令启动nacos服务 - 在应用销毁时自动执行docker-compose命令停止nacos服务- 添加@Slf4j注解以支持日志记录- 注释掉原有的SpringApplication.run()调用feat(camunda): 初始化camunda工作流引擎服务模块
    - 添加camunda工作流引擎核心依赖及相关配置- 配置MySQL数据库连接参数及camunda相关设置
    - 创建基础的camunda流程定义文件(process.bpmn)- 添加.gitignore和.gitattributes文件用于版本控制管理
    - 集成Spring Boot测试框架并创建基础测试类

---
+++ camunda-server/src/main/java/cn/crabapples/camunda/CamundaApplication.java
@@ -0,0 +1,13 @@
+package cn.crabapples.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(CamundaApplication.class, args);
+    }
+
+}
--- gateway/src/main/java/cn/crabapples/GatewayApplication.java
+++ gateway/src/main/java/cn/crabapples/GatewayApplication.java
@@ -1,20 +1,44 @@
 package cn.crabapples;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
 @SpringBootApplication
 public class GatewayApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+    public static void main(String[] args) throws IOException {
+        log.info("启动nacos服务...");
+        Process pwd = Runtime.getRuntime().exec("docker-compose -f ./docker-compose/nacos/docker-compose-nacos.yml up -d");
+        InputStream inputStream = pwd.getInputStream();
+        byte[] data = new byte[1024];
+        for (int i = inputStream.read(data); i != -1; i = inputStream.read(data)) {
+            System.out.print(new String(data, 0, i));
+        }
+//        SpringApplication.run(GatewayApplication.class, args);
     }
 
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @PreDestroy
+    void destroy() throws IOException {
+        log.info("关闭nacos服务");
+        Process pwd = Runtime.getRuntime().exec("docker-compose -f ./docker-compose/nacos/docker-compose-nacos.yml down");
+        InputStream inputStream = pwd.getInputStream();
+        byte[] data = new byte[1024];
+        for (int i = inputStream.read(data); i != -1; i = inputStream.read(data)) {
+            System.out.print(new String(data, 0, i));
+        }
     }
 }
