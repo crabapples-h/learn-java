8c1b771442c7c4dd81bc67ea430ab62c999fee27
mshe <666666666@666666666.666666666>
2025-11-20 05:47:40 +0800

    refactor(core): 移除不必要的注解依赖
    - 删除了 RestControllerAdvice 注解的导入
    - 简化了异常处理器的实现方式
    - 提升了代码的可维护性

--- module-base-core/src/main/java/cn/crabapples/common/CustomBlockExceptionHandler.java
+++ module-base-core/src/main/java/cn/crabapples/common/CustomBlockExceptionHandler.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
