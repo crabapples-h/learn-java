392383f986219632ed2eb1fbddc72ea443f05f0d
mshe <666666666@666666666.666666666>
2025-11-21 09:55:36 +0800

    feat(sentinel): 配置sentinel控制台地址并优化启动脚本
    - 修改bootstrap.yml中的sentinel控制台地址为172.16.8.70:8858
    - 更新docker-compose.yml中sentinel-dashboard的image版本至1.8.9
    - 在docker-compose.yml中添加nacos配置参数
    - 注释掉Dockerfile中的start.sh相关指令
    - 优化start.sh脚本，增加nacos配置参数并格式化命令行参数

--- docker-compose/sentinel-dashboard/start.sh
+++ docker-compose/sentinel-dashboard/start.sh
@@ -1,2 +1,13 @@
-#java -jar -Dserver.port=8858 -Dcsp.sentinel.dashboard.server=127.0.0.1:8858 -Dproject.name=sentinel-dashboard -Dsentinel.dashboard.auth.username=crabapples -Dsentinel.dashboard.auth.password=crabapples -Dauth.enabled=true -Dcsp.sentinel.log.dir=./logs/csp ./sentinel-dashboard-1.8.8.jar
-java -jar -Dserver.port=8858 -Dcsp.sentinel.dashboard.server=127.0.0.1:8858 -Dproject.name=sentinel-dashboard -Dsentinel.dashboard.auth.username=crabapples -Dsentinel.dashboard.auth.password=crabapples -Dauth.enabled=true -Dcsp.sentinel.log.dir=./logs/csp app.jar
+java -jar -Dserver.port=8858 \
+  -Dcsp.sentinel.dashboard.server=127.0.0.1:8858 \
+  -Dproject.name=sentinel-dashboard \
+  -Dsentinel.dashboard.auth.username=crabapples \
+  -Dsentinel.dashboard.auth.password=crabapples \
+  -Dauth.enabled=true \
+  -Dcsp.sentinel.log.dir=./logs/csp \
+  -Dnacos.config.server-addr=http://172.16.8.70:8848 \
+  -Dnacos.config.namespace=namespace-sentinel \
+  -Dnacos.config.groupId=DEFAULT_GROUP \
+  -Dnacos.config.username= \
+  -Dnacos.config.password= \
+  ./sentinel-dashboard-1.8.9.jar
