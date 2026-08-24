9abbc608d247474ce3b537f393d5f641628d66e0
mshe <666666666@666666666.666666666>
2026-08-24 17:03:07 +0800

    refactor(redis-server): 将redis-server模块迁移至modules-application并更新依赖
    - 将redis-server目录重命名为modules-application/redis-server
    - 为redis-server模块添加父项目配置
    - 更新embedded-redis依赖版本从0.6到0.7.3
    - 替换embedded-redis依赖的groupId从com.github.kstyrc到it.ozimov
    - 在modules-application/pom.xml中添加redis-server模块引用
    - 为RedisServerApplication类添加详细的TODO注释说明内嵌redis服务的限制

--- modules-application/redis-server/src/main/java/cn/crabapples/redis/RedisServerApplication.java
+++ modules-application/redis-server/src/main/java/cn/crabapples/redis/RedisServerApplication.java
@@ -4,6 +4,16 @@ import redis.embedded.RedisServer;
 
 import java.util.Scanner;
 
+/**
+ * TODO 内嵌redis服务
+ * 需要特定平台的的可执行文件, mac上测试无法启动, 也不推荐使用这种方式启动
+ *
+ * @author Mr.He
+ * 2021/4/10 15:33
+ * e-mail crabapples.cn@gmail.com
+ * qq 294046317
+ * pc-name mrhe
+ */
 public class RedisServerApplication {
     public static void main(String[] args) {
         System.out.println("start init inner redis server");
